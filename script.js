2ed5fb476f4e77753010d716a491a74e0bd02b65
Mayur Chopra <mayur5c@users.noreply.github.com>
2023-03-25 23:52:28 +0530

    Final Uploads

---
+++ script.js
@@ -0,0 +1,28 @@
+var lt = document.getElementById("lt");
+var lng = document.getElementById("lng");
+
+
+function getLocation() {
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+    var MAP = document.getElementById("map");
+    MAP.style.display = "block";
+  } else {
+    MAP.innerHTML = "API is not supported by the user's browser.";
+  }
+}
+
+function showPosition(position) {
+     lt.innerHTML = "Latitude: " + position.coords.latitude ;
+     lng.innerHTML = "Longitude:" + position.coords.longitude;
+    localStorage.setItem("Lat", lt.innerHTML);
+    localStorage.setItem("Long", lng.innerHTML);
+  }
+
+  function RemoveLocation(){
+    localStorage.removeItem("Lat")
+    localStorage.removeItem("Long")
+    lt.innerHTML="Latitude has been removed";
+    lng.innerHTML="Longitude has been removed";
+ }
